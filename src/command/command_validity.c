8d82cfb6768efda787de8bc12647991d217309f9
Benoit <benoit.hoffman@epitech.eu>
2017-05-11 08:42:36 +0000

    gestion d'erreur pour '&'

--- src/command/command_validity.c
+++ src/command/command_validity.c
@@ -5,7 +5,7 @@
 ** Login   <sahel.lucas-saoudi@epitech.eu>
 **
 ** Started on  Thu Apr 20 22:27:34 2017 Sahel Lucas--Saoudi
-** Last update Wed May 10 14:56:27 2017 Sahel Lucas--Saoudi
+** Last update Thu May 11 08:35:47 2017 Benoit Hoffman
 */
 
 #include "basic.h"
@@ -13,7 +13,7 @@
 
 static int	is_separator(char c)
 {
-  if (c == '|' || c == ';')
+  if (c == '|' || c == ';' || c == '&')
     return (1);
   return (0);
 }
@@ -22,14 +22,13 @@ static int	check_one_pipe(char *command, int i)
 {
   int		j;
 
-  j = i;
-  i++;
-  j--;
+  j = i - 1;
+  i += 1;
   while (command && command[i] && (command[i] == ' ' || command[i] == '\t'))
-    i++;
+    i += 1;
   while (command && j >= 0 && command[j] && (command[j] == ' ' ||
 					     command[j] == '\t'))
-    j--;
+    j -= 1;
   if (j < 0 || !command[i] || is_separator(command[i]) ||
       is_separator(command[j]))
     return (1);
@@ -43,11 +42,12 @@ static int	pipe_validity(char *command)
   i = 0;
   while (command && command[i])
     {
-      if (!match(&command[i], "||*") && command[i] == '|' && check_one_pipe(command, i))
+      if (!match(&command[i], "||*") && command[i] == '|'
+	  && check_one_pipe(command, i))
 	return (1);
       if (match(&command[i], "||*"))
-	i++;
-      i++;
+	i += 1;
+      i += 1;
     }
   return (0);
 }
@@ -61,6 +61,12 @@ static int	 check_flex_redir(char *command)
 
 int	check_flexibility(t_shell *shell)
 {
+  if (match(shell->command, "*&*") && !match(shell->command, "*&&*"))
+    {
+      putstr_("\'&\': Work in progress\n", 2);
+      shell->status = 1;
+      return (1);
+    }
   if (pipe_validity(shell->command) || check_flex_redir(shell->command))
     {
       putstr_("Invalid null command.\n", 2);
