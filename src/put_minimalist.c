ef228613c74334c5a8572a806a5fed12f1fac2fa
Benoit <benoit.hoffman@epitech.eu>
2017-05-13 09:49:17 +0000

    put_minimalist encore

--- src/put_minimalist.c
+++ src/put_minimalist.c
@@ -5,7 +5,7 @@
 ** Login   <benoit.hoffman@epitech.eu>
 ** 
 ** Started on  Fri May 12 15:01:52 2017 Benoit Hoffman
-** Last update Sat May 13 09:10:59 2017 Benoit Hoffman
+** Last update Sat May 13 09:48:58 2017 Benoit Hoffman
 */
 
 #include	<string.h>
@@ -19,22 +19,21 @@ int		put_minimalist(t_shell *shell)
 
   tmp = getvar(shell->env, "PATH=*");
   shell->path = parse_(tmp, ':');
-  free(tmp);
   i = 0;
-  if (!shell->path)
-    shell->path = malloc(sizeof(char *) * 3);
-  else
+  if (!tmp)
     {
-      while (shell->path[i])
-	i += 1;
-      shell->path = realloc(shell->path, sizeof(char *) * (i + 3));
+      if (!shell->path)
+	{
+	  shell->path = malloc(sizeof(char *) * 3);
+	  if (!shell->path)
+	    return (84);
+	  shell->path[i++] = strdup("/usr/bin");
+	  shell->path[i++] = strdup("/bin");
+	  shell->path[i] = NULL;
+	  if (!shell->path[i - 1] || !shell->path[i - 2])
+	    return (84);
+	}
     }
-  if (!shell->path)
-    return (84);
-  shell->path[i++] = strdup("/usr/bin");
-  shell->path[i++] = strdup("/bin");
-  shell->path[i] = NULL;
-  if (!shell->path[i - 1] || !shell->path[i - 2])
-    return (84);
+  free(tmp);
   return (0);
 }
